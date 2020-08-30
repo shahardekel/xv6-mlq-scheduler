def5416bbb1c292e7ce0f510ef1a298e87dc1ecc
Shahar Dekel <69580046+shahardekel@users.noreply.github.com>
2020-08-30 20:28:51 +0300

    Add files via upload

---
+++ set_priority.c
@@ -0,0 +1,14 @@
+// this is a place holder for set_priority(int) that you have to implement.
+// replace the content of this file with your implementation,
+// or just delete it, and write the function where you want
+
+/*#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+int set_priority(int prio){
+   setpriority(prio);
+   return 1;
+}*/
+
---
+++ sysproc.c
@@ -0,0 +1,123 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int
+sys_halt(void)
+{
+  outw(0x604, 0x2000);
+  return 0;
+}
+
+int
+sys_setpriority(void)
+{
+  int prio;
+
+  if(argint(0, &prio) < 0)
+    return -1;
+  return setpriority(prio);
+}
+
+int sys_wait2(void) {
+  int *retime, *rutime, *stime, *elapsed;
+  if (argptr(0, (void*)&retime, sizeof(retime)) < 0)
+    return -1;
+  if (argptr(1, (void*)&rutime, sizeof(retime)) < 0)
+    return -1;
+  if (argptr(2, (void*)&stime, sizeof(stime)) < 0)
+    return -1;
+  if (argptr(3, (void*)&elapsed, sizeof(elapsed)) < 0)
+    return -1;
+  return wait2(retime, rutime, stime, elapsed);
+}
+
+
